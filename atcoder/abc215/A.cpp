c3fbca9913de359be4ff73112a9285075bf00f17
Md. Asadujjaman Noor <asadujjaman1122@gmail.com>
2021-08-21 18:03:00 +0600

    Add solution for problem `A - Your First Judge` Link: https://atcoder.jp/contests/abc215/tasks/abc215_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc215/submissions/25197894

---
+++ atcoder/abc215/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a;
+    cin>>a;
+    if(a=="Hello,World!")cout<<"AC"<<endl;
+    else cout<<"WA"<<endl;
+
+    return 0;
+}
+
+
+
