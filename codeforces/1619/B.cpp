157a55e74e3b9c1b2058dd2d457eaccadd97ff62
Md. Asadujjaman Noor <asadujjaman1122@gmail.com>
2021-12-20 23:15:00 +0600

    Add solution for problem `B - Squares and Cubes` Link: https://codeforces.com/contest/1619/problem/B Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1619/submission/140096425

--- codeforces/1619/B.cpp
+++ codeforces/1619/B.cpp
@@ -10,8 +10,13 @@ int main()
    {
        long long a;
        cin>>a;
-       int s=(int)sqrt(a);
-       long long s1=(int)cbrt(a); 
+       int s=(int)pow(a,0.5);
+       long long s1=(int)pow(a,0.33);
+       while (1)
+       {
+           if((s1*s1*s1)>a){s1--;break;}
+           s1++;
+       }
        long b=sqrt(s1);
        cout<<s1+s-b<<endl;
               
