30cfb16765bb76ee50a1f1350419122d085992da
Md. Asadujjaman Noor <asadujjaman1122@gmail.com>
2022-01-11 23:34:00 +0600

    Add solution for problem `B - Make AP` Link: https://codeforces.com/contest/1624/problem/B Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1624/submission/142401172

---
+++ codeforces/1624/B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--) {
+		int a,b,c;
+		cin>>a>>b>>c;
+		if((a+c)%(2*b)==0&&a+c>0||((2*b-c)%a==0&&2*b-c>0)||((2*b-a)%c==0&&2*b-a>0))
+			puts("YES");
+		else
+			puts("NO");
+	}
+} 
