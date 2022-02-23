75a5c85c169ef640f368adfe2ebed5b2c121379d
Md. Asadujjaman Noor <asadujjaman1122@gmail.com>
2022-02-23 12:52:00 +0600

    Add solution for problem `A - Doors and Keys` Link: https://codeforces.com/contest/1644/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1644/submission/147398673

--- codeforces/1644/A.cpp
+++ codeforces/1644/A.cpp
@@ -9,7 +9,13 @@ int main()
         cin>>s;
         int c=0;
         int a[200]={0};
-
+        for (int i = 0; i < s.size()-1; i++)
+        {
+            if(s[i]==s[i+1]+32){c++;continue;}    
+        }
+        if(c==s.size()/2)cout<<"YES"<<endl;
+        else{
+            c=0;
             for (int i = 0; i < s.size(); i++)
             {
                 if(s[i]>='a')a[s[i]]++;
@@ -19,6 +25,9 @@ int main()
              
         if(c==1) cout<<"NO"<<endl;
         else cout<<"YES"<<endl;
+        }
+
+        
     }
     return 0;
 }
